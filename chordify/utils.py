dd6ae884d7ae4cdefeef1dc6fa24efd2ecfd14c1
Matúš Škerlík <hernyucet11@gmail.com>
2020-02-18 18:00:05 +0100

    + score, fixed

--- chordify/utils.py
+++ chordify/utils.py
@@ -32,7 +32,7 @@ from pathlib import Path
 from typing import Tuple
 
 from .annotation import ChordTimeline
-from .music import IChord
+from .music import IChord, ChordKey
 
 
 class SupervisedDirectoryAdapter(object):
@@ -66,11 +66,17 @@ class SupervisedDirectoryAdapter(object):
 
 def score(prediction: ChordTimeline, annotation: ChordTimeline) -> float:
     _correct = 0
+    _skip = 0
     _i_annotation = 0
     _i_prediction = 0
     while _i_annotation < len(annotation) and _i_prediction < len(prediction):
         start, stop, chord = annotation[_i_annotation]
         p_start, p_stop, p_chord = prediction[_i_prediction]
+
+        if chord == IChord(ChordKey.N, None):
+            _i_annotation += 1
+            _skip += 1
+            continue
 
         if p_stop < start:
             _i_prediction += 1
@@ -88,4 +94,4 @@ def score(prediction: ChordTimeline, annotation: ChordTimeline) -> float:
         elif p_start <= start and p_stop >= stop and chord == p_chord:
             _correct += (stop - start) / (p_stop - p_start)
         _i_prediction += 1
-    return _correct / len(annotation)
+    return _correct / (len(annotation) - _skip)
